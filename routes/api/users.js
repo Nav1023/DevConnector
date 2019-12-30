fa9ee2ed915b3c2ae837f0c388be511ea8d54ebe
Navneet Singh <rathore.singhnav@gmail.com>
2019-12-31 01:07:45 +0530

    Added jwt token (#4)

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express =  require('express');
 const router = express.Router();
 const gravatar =  require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const  { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -61,8 +63,22 @@ router.post('/',
         await  user.save();
 
         //Return JWT
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token)=>{
+                if(err) throw err;
+                res.status(200).send({token});
+            }
+            )
 
-        return res.status(200).send('User Registered');
+        // return res.status(200).send('User Registered');
         } catch(err){
         console.log(err);
         return res.status(500).send('Server Error');
