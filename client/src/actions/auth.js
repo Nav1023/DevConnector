38dea656484060a6120830307c85bbfb55a10115
Navneet Singh <rathore.singhnav@gmail.com>
2020-01-16 22:22:49 +0530

    Added the create profile page with spinner

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
     AUTH_ERROR,
     LOGIN_FAIL,
     LOGIN_SUCCESS,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -89,4 +90,5 @@ export const login = (email, password) => async dispatch => {
 //Logout
 export const logout = ()  => dispatch => {
     dispatch({ type: LOGOUT });
+    dispatch({ type: CLEAR_PROFILE });
 }
