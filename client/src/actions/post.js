45d4d6e72e137723982ee0e5803684a712e9bd95
Navneet Singh <rathore.singhnav@gmail.com>
2020-03-16 00:18:00 +0530

    component for adding the comment

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types'
 
 //GET POSTS 
@@ -123,3 +125,48 @@ export const getPost = id => async dispatch => {
         });
     }
 }
+
+//Add COMMENT
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        headers : {
+         'Content-Type': 'application/json'
+        }
+    }
+    console.log("postId", postId);
+    console.log("formData", formData);
+    try {
+        const res = await axios.post(`http://localhost:5000/api/posts/comment/${postId}`,formData, config);
+       
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+        dispatch(setAlert('Comment Added ', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
+//DELETE COMMENT
+export const deleteComment= (postId, commentId) => async dispatch => {
+
+    try {
+        const res = await axios.delete(`http://localhost:5000/api/posts/comment/${postId}/${commentId}`);
+       
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+        dispatch(setAlert('Comment Removed ', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status}
+        });
+    }
+}
+
