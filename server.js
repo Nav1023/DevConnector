f7677413bf638c187c86ab5d222897aa922c2555
Navneet Singh <rathore.singhnav@gmail.com>
2020-03-16 23:20:42 +0530

    Made changes for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -10,13 +11,22 @@ connectDB();
 //Init  Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//Serve static assests in production
+if(process.env.NODE_ENV === 'production'){
+    //Set Static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        app.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
